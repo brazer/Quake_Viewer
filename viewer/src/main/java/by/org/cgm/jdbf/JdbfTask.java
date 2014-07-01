e7b6a25d9a255a49258aee8c426967293c2191bc
brazer <salanevich.anatol@gmail.com>
2014-07-01 12:08:29 +0300

    Revert "Update Android Studio"
    This reverts commit 72a6db34f37f6d0444231ac10fa434c80f596d28.

--- viewer/src/main/java/by/org/cgm/jdbf/JdbfTask.java
+++ viewer/src/main/java/by/org/cgm/jdbf/JdbfTask.java
@@ -171,6 +171,7 @@ public class JdbfTask extends AsyncTask<String, String, Boolean>
     public static class QuakeRecordEarth extends QuakeRecord
     {
         public String N, DateTime, Lon, Lat, Depth, MPSP, MPLP, MS, LocRus, Loc;
+
         public void setField(int nField, String value) {
             switch (nField) {
                 case 0:
